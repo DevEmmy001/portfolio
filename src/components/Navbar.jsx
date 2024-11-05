13e218d1f387340a3f3f38b411861f5f82638498
Emeka Victor <chukwuemekav420@gmail.com>
2024-11-05 16:16:54 +0100

    project startout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-auto bg-slate-400 flex justify-between p-4">
+      {/* my portfolio's logo icon */}
+      <div id="navLogoIcon" className="">
+        Logo
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
