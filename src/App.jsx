4acd83e8eda634db63ba4ce2af8a408f14c0f5e3
Emeka Victor <chukwuemekav420@gmail.com>
2024-11-08 17:45:32 +0100

    added header content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   );
 }
