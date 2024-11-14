21c490cba87f798710fafbf2932bfa4925d557ff
Emeka Victor <chukwuemekav420@gmail.com>
2024-11-14 06:57:42 +0100

    uploaded for future updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "./index.css";
 import WorkCard from "./components/WorkCard";
+import { ArrowBigLeftDashIcon } from "lucide-react";
 
 function App() {
   return (
-    <main className="bg-blue-700 text-xs p-4 md:flex">
-      <WorkCard />
-      <WorkCard />
-      <WorkCard />
-      <WorkCard />
-      <WorkCard />
+    <main className="bg-blue-700 font-PS h-full ">
+      <div className=" text-xs p-4 md:flex w-full justify-center">
+        {" "}
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+      </div>
+      <a href="">
+        <button className="bg-yellow-400 w-full flex items-center justify-center text-sm py-3 px-4 hover:text-white sticky top-0 bottom-0 shadow-md">
+          <ArrowBigLeftDashIcon strokeWidth={1} width={25} /> Go Back{" "}
+        </button>
+      </a>
     </main>
   );
 }
